8975532bbdabec492681c0464ad783e2ba6afa3c
Kocsis Gábor <ifjkocsis.gabor@gmail.com>
2021-10-17 11:35:08 +0200

    Add JS, About page comlete

---
+++ html-feladatok-bemutatkozo/about.js
@@ -0,0 +1,16 @@
+function validateForm() {
+    let name = document.querySelector('#name');
+    let email = document.querySelector('#email');
+    let message = document.querySelector('#message');
+
+    if (name.value.length < 5) {
+        alert('A név nem lehet kevesebb mint 5 karakter');
+    } else if (((email.value.indexOf("@")) || (email.value.indexOf("."))) < 0) {
+        alert('Az email cím nem érvényes');
+    } else if (message.value.length < 20) {
+        alert('Az üzenet nem lehet kevesebb mint 20 karakter');
+    } else {
+        alert('minden ok')
+    }
+}
+
